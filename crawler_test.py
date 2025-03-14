92d63cb4e3a4c090c688668b3cb0b9a4a0a84f04
JidaDiao <1269548817@qq.com>
2025-03-14 14:49:30 +0800

    111

--- crawler_test.py
+++ crawler_test.py
@@ -7,9 +7,7 @@ import logging
 # 配置日志
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-config.use_qr_code = False
-config.phonenumber = "13958853656"
-config.password = "12345ssdlh"
+
 # 使用自动管理 ChromeDriver 版本的工厂
 WebDriverFactory = WebDriverFactoryCreator().create_factory(headless=False)
 
